cbd377cff0f3952af22ec86c72280c0d9c24362d
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-14 20:03:56 -0300

    feat: should thrown an error if the bimester input to create entity isn't right

--- src/entity/biemonthly-result.ts
+++ src/entity/biemonthly-result.ts
@@ -1,8 +1,29 @@
 import { type BiemonthlyResultInput } from '../protocols/biemonthly-result-input'
 
 export class BiemonthlyResult {
-  // private readonly bimester: Bimestre
-  // private
-  // constructor () {}
-  public create (bimesterInfo: BiemonthlyResultInput): void {}
+  private _bimester: string
+  private _discipline: string
+  private _grade: number
+
+  constructor () {
+    this._bimester = ''
+    this._discipline = ''
+    this._grade = 0
+  }
+
+  public create (bimesterInfo: BiemonthlyResultInput): void {
+    this.validations(bimesterInfo)
+    this._bimester = bimesterInfo.bimester
+    this._discipline = bimesterInfo.discipline
+    this._grade = bimesterInfo.grade
+  }
+
+  public validations (bimesterInfo: BiemonthlyResultInput): void {
+    const bimesters = ['PRIMEIRO', 'SEGUNDO', 'TERCEIRO', 'QUARTO']
+    // const disciplines = ['Biologia', 'Artes', 'Geografia', 'Sociologia']
+
+    if (!bimesters.some((bimester) => bimester === bimesterInfo.bimester)) {
+      throw new Error()
+    }
+  }
 }
