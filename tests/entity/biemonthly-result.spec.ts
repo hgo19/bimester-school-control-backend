4cf133ae204d993d3a19631c2017edc6703a2360
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-14 20:22:29 -0300

    test: throws an error if the grade input isn't right

--- tests/entity/biemonthly-result.spec.ts
+++ tests/entity/biemonthly-result.spec.ts
@@ -9,7 +9,7 @@ describe('BiemonthlyResult Entity', () => {
     const input: BiemonthlyResultInput = {
       bimester: 'QUINTO',
       discipline: 'Geografia',
-      grade: 5
+      grade: 0
     }
 
     expect(() => { sut.create(input) }).toThrow(new InvalidParams("Bimester must be one of following: 'PRIMEIRO', 'SEGUNDO', 'TERCEIRO', 'QUARTO'"))
@@ -21,9 +21,21 @@ describe('BiemonthlyResult Entity', () => {
     const input: BiemonthlyResultInput = {
       bimester: 'PRIMEIRO',
       discipline: 'Climatologia',
-      grade: 5
+      grade: 10
     }
 
     expect(() => { sut.create(input) }).toThrow(new InvalidParams("Discipline must be one of following: 'Biologia', 'Artes', 'Geografia', 'Sociologia'"))
+  })
+
+  test("2. should throw an error if the discipline input isn't right", () => {
+    // System under test
+    const sut = new BiemonthlyResult()
+    const input: BiemonthlyResultInput = {
+      bimester: 'PRIMEIRO',
+      discipline: 'Geografia',
+      grade: 15
+    }
+
+    expect(() => { sut.create(input) }).toThrow(new InvalidParams('Grade value must be between 0 and 10'))
   })
 })
