c01159da932073ed9b1be62eecaa725087c10e4e
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-14 19:07:26 -0300

    refactor: change BiemonthlyResultInput interface types of bimester and discipline

--- src/protocols/biemonthly-result-input.ts
+++ src/protocols/biemonthly-result-input.ts
@@ -1,8 +1,5 @@
-type bimesterType = 'PRIMEIRO' | 'SEGUNDO' | 'TERCEIRO' | 'QUARTO'
-type disciplineType = 'Biologia' | 'Artes' | 'Geografia' | 'Sociologia'
-
 export interface BiemonthlyResultInput {
-  bimester: bimesterType
-  discipline: disciplineType
+  bimester: string
+  discipline: string
   grade: number
 }
