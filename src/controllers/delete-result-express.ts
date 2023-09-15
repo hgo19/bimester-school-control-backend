fa0282680127245c0881b91c8a2b4f9ddd3893a0
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 18:02:43 -0300

    feat: ensure DeleteResultExpress controller is working

---
+++ src/controllers/delete-result-express.ts
@@ -0,0 +1,19 @@
+import { type RemoveResult } from '../services/remove-result'
+import { type NextFunction, type Request, type Response } from 'express'
+
+export class DeleteResultExpress {
+  private readonly _service: RemoveResult
+  constructor (service: RemoveResult) {
+    this._service = service
+  }
+
+  execute = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const { id } = req.body
+      await this._service.execute(id)
+      res.status(204).json({ message: 'result deleted.' })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
