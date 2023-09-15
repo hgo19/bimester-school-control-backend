51e39a543389e945bcecfd60d6140e2a842f3fa3
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 14:10:44 -0300

    feat: create error-hanlders

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,20 @@
+import { type NextFunction, type Request, type Response } from 'express'
+import { InvalidParams } from '../exceptions/invalid-param'
+import { AbstractHttpError, BadRequestError, InternalServerError } from '../utils/http-erros'
+import { DisciplineAlreadyExistError } from '../exceptions/discipline-already-exist'
+
+export const errorTreatment = (error: Error, _req: Request, _res: Response, next: NextFunction): any => {
+  if (error instanceof InvalidParams || error instanceof DisciplineAlreadyExistError) {
+    next(new BadRequestError(error.message))
+  } else if (error instanceof AbstractHttpError) {
+    next(error)
+  } else {
+    console.log(error)
+    next(new InternalServerError('Unexpected Error Happend'))
+  }
+}
+
+export const errorHandler = (error: AbstractHttpError, _req: Request, res: Response, _next: NextFunction): any => {
+  const { message, statusCode } = error
+  return res.status(statusCode).json({ error: message })
+}
