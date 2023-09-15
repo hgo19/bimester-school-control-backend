cc6c0fbc675d68a248e09a2d0ad8d19fde1f7ca2
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 13:44:22 -0300

    feat: ensure response is called with the right values in AddResultExpress Controller

--- src/controllers/add-result-express.ts
+++ src/controllers/add-result-express.ts
@@ -1,6 +1,15 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { type CreateResult } from '../services/create-result'
 
+interface ObjectResponse {
+  id: string
+  bimestre: string
+  disciplina: string
+  nota: number
+  criadoEm: string
+  atualizadoEm?: string
+}
+
 export class AddResultExpress {
   private readonly _service: CreateResult
   constructor (service: CreateResult) {
@@ -10,7 +19,20 @@ export class AddResultExpress {
   execute = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
       const { bimester, discipline, grade } = req.body
-      this._service.execute({ bimester, discipline, grade })
+      const createdResult = await this._service.execute({ bimester, discipline, grade })
+      const result: ObjectResponse = {
+        id: createdResult.id,
+        bimestre: createdResult.bimester,
+        disciplina: createdResult.discipline,
+        nota: createdResult.grade,
+        criadoEm: createdResult.createdAt
+      }
+
+      if (createdResult.updatedAt !== undefined) {
+        result.atualizadoEm = createdResult.updatedAt
+      }
+
+      return res.status(201).json(result)
     } catch (error) {
       next(error)
     }
