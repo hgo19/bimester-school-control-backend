15bb74807e1ffbee1672ddacba3729f9fbb99115
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-14 21:04:19 -0300

    test: refactor tests and put sut in a factory method

--- tests/services/create-result.spec.ts
+++ tests/services/create-result.spec.ts
@@ -1,29 +1,32 @@
 import { BiemonthlyResult } from '../../src/entity/biemonthly-result'
-import { type BiemonthlyResultInput } from '../../src/protocols/biemonthly-result-input'
-import { type BiemonthlyResultOutput } from '../../src/protocols/biemonthly-result-output'
-import { type BiemonthlyResultRepository } from '../../src/protocols/biemonthly-result-repository'
 import { CreateResult } from '../../src/services/create-result'
+import { BiemonthlyRepositoryStub } from '../mocks/biemonthly-result-repository-mock'
+import { type BiemonthlyResultRepository } from '../../src/protocols/biemonthly-result-repository'
+import { type BiemonthlyResultInput } from '../../src/protocols/biemonthly-result-input'
+
+interface SutTypes {
+  repository: BiemonthlyResultRepository
+  entity: BiemonthlyResult
+  sut: CreateResult
+}
+
+const makeSut = (): SutTypes => {
+  const repository = new BiemonthlyRepositoryStub()
+  const entity = new BiemonthlyResult()
+  const sut = new CreateResult(entity, repository)
+
+  return {
+    repository,
+    entity,
+    sut
+  }
+}
 
 describe('CreateResult Service', () => {
   test('1. should calls create method of BiemonthlyResult entity with right values', async () => {
     // System under test
-    class RepositoryStub implements BiemonthlyResultRepository {
-      async create (input: BiemonthlyResultInput): Promise<BiemonthlyResultOutput> {
-        return await new Promise(resolve => {
-          resolve({
-            id: '1',
-            bimester: 'PRIMEIRO',
-            discipline: 'Geografia',
-            grade: 5,
-            createdAt: '2023-09-14 10:30:00'
-          })
-        })
-      }
-    }
-    const biemonthlyResult = new BiemonthlyResult()
-    const repositoryStub = new RepositoryStub()
-    const sut = new CreateResult(biemonthlyResult, repositoryStub)
-    const createBiemonthlyResultSpy = jest.spyOn(biemonthlyResult, 'create')
+    const { sut, entity } = makeSut()
+    const createBiemonthlyResultSpy = jest.spyOn(entity, 'create')
     const input: BiemonthlyResultInput = {
       bimester: 'PRIMEIRO',
       discipline: 'Geografia',
@@ -36,23 +39,8 @@ describe('CreateResult Service', () => {
 
   test('2. should calls create method of BiemonthlyResult repository with right values', async () => {
     // System under test
-    class RepositoryStub implements BiemonthlyResultRepository {
-      async create (input: BiemonthlyResultInput): Promise<BiemonthlyResultOutput> {
-        return await new Promise(resolve => {
-          resolve({
-            id: '1',
-            bimester: 'PRIMEIRO',
-            discipline: 'Geografia',
-            grade: 5,
-            createdAt: '2023-09-14 10:30:00'
-          })
-        })
-      }
-    }
-    const biemonthlyResult = new BiemonthlyResult()
-    const repositoryStub = new RepositoryStub()
-    const sut = new CreateResult(biemonthlyResult, repositoryStub)
-    const repositoryStubSpy = jest.spyOn(repositoryStub, 'create')
+    const { sut, repository } = makeSut()
+    const repositoryStubSpy = jest.spyOn(repository, 'create')
     const input: BiemonthlyResultInput = {
       bimester: 'PRIMEIRO',
       discipline: 'Geografia',
