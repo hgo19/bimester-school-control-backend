6ee3854cf86a3b00e15cb4c9f6aadfa92700d2d9
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 16:40:33 -0300

    refactor: remove unecessary lines from add-result-express

--- src/controllers/add-result-express.ts
+++ src/controllers/add-result-express.ts
@@ -1,15 +1,6 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { type CreateResult } from '../services/create-result'
 
-interface ObjectResponse {
-  id: string
-  bimestre: string
-  disciplina: string
-  nota: number
-  criadoEm: string
-  atualizadoEm?: string
-}
-
 export class AddResultExpress {
   private readonly _service: CreateResult
   constructor (service: CreateResult) {
@@ -20,16 +11,13 @@ export class AddResultExpress {
     try {
       const { bimester, discipline, grade } = req.body
       const createdResult = await this._service.execute({ bimester, discipline, grade })
-      const result: ObjectResponse = {
+      const result = {
         id: createdResult.id,
         bimestre: createdResult.bimester,
         disciplina: createdResult.discipline,
         nota: createdResult.grade,
-        criadoEm: createdResult.createdAt
-      }
-
-      if (createdResult.updatedAt !== undefined) {
-        result.atualizadoEm = createdResult.updatedAt
+        criadoEm: createdResult.createdAt,
+        atualizadoEm: createdResult.updatedAt
       }
 
       return res.status(201).json(result)
