18c5948a9bd3af471bf911451c26e5aca55caedc
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 00:23:04 -0300

    test: update BiemonthlyResultRepositoryStub to fit with the repository interface

--- tests/mocks/biemonthly-result-repository-mock.ts
+++ tests/mocks/biemonthly-result-repository-mock.ts
@@ -1,6 +1,18 @@
 import { type BiemonthlyResultInput, type BiemonthlyResultOutput, type BiemonthlyResultRepository } from '../../src/protocols/'
 
 export class BiemonthlyRepositoryStub implements BiemonthlyResultRepository {
+  async findOne (id: string): Promise<BiemonthlyResultOutput> {
+    return await new Promise(resolve => {
+      resolve({
+        id,
+        bimester: 'PRIMEIRO',
+        discipline: 'Geografia',
+        grade: 5,
+        createdAt: '2023-09-14 10:30:00'
+      })
+    })
+  }
+
   async create (input: BiemonthlyResultInput): Promise<BiemonthlyResultOutput> {
     return await new Promise(resolve => {
       resolve({
