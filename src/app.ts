7f7f1e12a19a74584ad175fbb3da749ee9ff4f6b
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 18:19:48 -0300

    feat: create delete-result route and implements it

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import createResultRoute from './routes/create-result.routes'
 import { errorHandler, errorTreatment } from './middlewares/error-handler'
 import getAllResultsRoute from './routes/get-all-result.routes'
+import deleteResultRoute from './routes/delete-result.routes'
 
 const app = express()
 app.use(cors())
@@ -10,6 +11,7 @@ app.use(express.json())
 
 app.use('/createresult', createResultRoute)
 app.use('/listall', getAllResultsRoute)
+app.use('/deleteresult', deleteResultRoute)
 
 app.use(errorTreatment)
 app.use(errorHandler)
--- src/middlewares/validations.ts
+++ src/middlewares/validations.ts
@@ -12,3 +12,17 @@ export const inputValidations = (req: Request, res: Response, next: NextFunction
     next(error)
   }
 }
+
+export const idBodyValidations = (req: Request, res: Response, next: NextFunction): any => {
+  try {
+    const { id } = req.body
+    if (id === false) {
+      throw new BadRequestError("You need to put 'id' on your body request.")
+    } else if (typeof id !== 'string') {
+      throw new BadRequestError("The 'id' property must be a string.")
+    }
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
