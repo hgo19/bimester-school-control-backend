066d42a5b4a8e574baff0b9c32cb51c5e575f8ec
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 22:01:37 -0300

    test: change remove result tests

--- tests/services/remove-result.spec.ts
+++ tests/services/remove-result.spec.ts
@@ -6,11 +6,11 @@ describe('RemoveResult Service', () => {
     // System under test
     const repo = new BimesterRepositoryStub()
     const sut = new RemoveResult(repo)
-    const delteSpy = jest.spyOn(repo, 'delete')
+    const deleteSpy = jest.spyOn(repo, 'delete')
 
     await sut.execute('1')
 
-    expect(delteSpy).toHaveBeenCalledWith('1')
+    expect(deleteSpy).toHaveBeenCalledWith('1')
   })
 
   test('2. should throws if repository throws', async () => {
@@ -20,24 +20,5 @@ describe('RemoveResult Service', () => {
     jest.spyOn(repo, 'delete').mockImplementation(() => { throw new Error() })
 
     await expect(sut.execute('invalid_id')).rejects.toThrow()
-  })
-
-  test("3. should throws doesn't delete a result in db", async () => {
-    // System under test
-    const repo = new BimesterRepositoryStub()
-    jest.spyOn(repo, 'delete').mockImplementation(async () => { return await new Promise(resolve => { resolve(false) }) })
-    const sut = new RemoveResult(repo)
-
-    await expect(sut.execute('2')).rejects.toThrow()
-  })
-
-  test('4. should returns true on success', async () => {
-    // System under test
-    const repo = new BimesterRepositoryStub()
-    const sut = new RemoveResult(repo)
-
-    const response = await sut.execute('1')
-
-    expect(response).toBeTruthy()
   })
 })
