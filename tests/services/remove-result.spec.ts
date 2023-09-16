752ce51a0ef826aef6e372563bce8cfc127aaa44
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-16 14:12:30 -0300

    test: update remove result service tests to coverage the updates

--- tests/services/remove-result.spec.ts
+++ tests/services/remove-result.spec.ts
@@ -2,7 +2,28 @@ import { RemoveResult } from '../../src/services/remove-result'
 import { BimesterRepositoryStub } from '../mocks/bimester-result-repository-stub'
 
 describe('RemoveResult Service', () => {
-  test('1. should calls repository with the right value', async () => {
+  test('1. should calls findOne method repository with the right value', async () => {
+    // System under test
+    const repo = new BimesterRepositoryStub()
+    const sut = new RemoveResult(repo)
+    const findOneSpy = jest.spyOn(repo, 'findOne')
+
+    await sut.execute('1')
+
+    expect(findOneSpy).toHaveBeenCalledWith(1)
+  })
+
+  test("2. should throws an error if an object isn't found in db", async () => {
+    // System under test
+    const repo = new BimesterRepositoryStub()
+    const sut = new RemoveResult(repo)
+
+    jest.spyOn(repo, 'findOne').mockImplementation(async () => { return await new Promise(resolve => { resolve(null) }) })
+
+    await expect(sut.execute('1')).rejects.toThrow()
+  })
+
+  test('3. should calls delete method repository with the right value', async () => {
     // System under test
     const repo = new BimesterRepositoryStub()
     const sut = new RemoveResult(repo)
@@ -13,7 +34,7 @@ describe('RemoveResult Service', () => {
     expect(deleteSpy).toHaveBeenCalledWith('1')
   })
 
-  test('2. should throws if repository throws', async () => {
+  test('3. should throws if repository throws', async () => {
     // System under test
     const repo = new BimesterRepositoryStub()
     const sut = new RemoveResult(repo)
