40c1fbca6cf2e08dc982ece0033a764794cf47a0
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-16 15:01:35 -0300

    test: when u try to list by bimester sholud throws an error if none of entities where found in db

--- tests/services/list-by-bimester.spec.ts
+++ tests/services/list-by-bimester.spec.ts
@@ -25,4 +25,14 @@ describe('ListByBimester Service', () => {
       updatedAt: '2023-09-14 10:30:00'
     }])
   })
+
+  test('2. should throws an error if none of entities where found in db', async () => {
+    // System under test
+    const repository = new BimesterRepositoryStub()
+    const sut = new ListByBimester(repository)
+    jest.spyOn(repository, 'getByBimester').mockImplementation(async () => { return await new Promise(resolve => { resolve(null) }) })
+    const bimester = 'invalid_bimester'
+
+    await expect(sut.execute(bimester)).rejects.toThrow()
+  })
 })
