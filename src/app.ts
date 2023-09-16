f5d686a57a69eabba6afd8d5cc502aab47d5ad91
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 22:07:18 -0300

    refactor: remove unecessary comments

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-// import createResultRoute from './routes/create-result.routes'
 import { errorHandler, errorTreatment } from './middlewares/error-handler'
-// import getAllResultsRoute from './routes/get-all-result.routes'
-// import deleteResultRoute from './routes/delete-result.routes'
 import resultsRoutes from './routes/result.routes'
 
 const app = express()
@@ -11,10 +8,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/results', resultsRoutes)
-
-// app.use('/createresult', createResultRoute)
-// app.use('/listall', getAllResultsRoute)
-// app.use('/deleteresult', deleteResultRoute)
 
 app.use(errorTreatment)
 app.use(errorHandler)
