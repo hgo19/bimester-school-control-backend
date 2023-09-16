7a4b7d4fd726670a5752470f877e6eb48b527ba3
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-15 21:02:23 -0300

    chore: update jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/app.ts',
+    '!<rootDir>/src/server.ts',
+    '!<rootDir>/src/database/**/*.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
